65df2341f38265b7e5c51715135247382b8fec3f
pashgun <36420775+pashgun@users.noreply.github.com>
2018-02-13 02:54:38 +0300

    Update object_manager.js

--- object_manager.js
+++ object_manager.js
@@ -2,7 +2,7 @@ ymaps.ready(init);
 
 function init () {
     var myMap = new ymaps.Map('map', {
-            center: [55.76, 37.64],
+            center: [37.64, 55.10],
             zoom: 15
         }, {
             searchControlProvider: 'yandex#search'
