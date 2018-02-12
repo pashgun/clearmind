f8146b27fbb9a681b0077134a69f1d6fdb6ae6b1
pashgun <36420775+pashgun@users.noreply.github.com>
2018-02-13 02:02:18 +0300

    Update object_manager.js

--- object_manager.js
+++ object_manager.js
@@ -17,8 +17,8 @@ function init () {
 
     // Чтобы задать опции одиночным объектам и кластерам,
     // обратимся к дочерним коллекциям ObjectManager.
-    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
-    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    objectManager.objects.options.set('preset', 'islands#redDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#redClusterIcons');
     myMap.geoObjects.add(objectManager);
 
     $.ajax({
